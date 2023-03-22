ce2462e1e3a29dfb79bf6b5acd3a526632f5b894
somura asuka <104813987+somura9@users.noreply.github.com>
2023-03-22 11:29:41 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-import "./styles.css";
+/**
+ * const let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var val1 = "var変数";
+console.log(val1);
+
+val1 = "var変数改変後";
+console.log(val1);
+
+var val1 = "var変数を再宣言";
+console.log(val1);
+
+//letは上書き可能
+let val2 = "let変数";
+console.log(val2);
+
+val2 = "let変数を上書き";
+console.log(val2);
+
+// constは上書きも不可能
+const val3 = "const変数";
+console.log(val3);
+
+/**
+ * オブジェクト
+ */
+
+// constで定義していてもオブジェクトは上書きできる
+const val4 = {
+  name: "somura",
+  age: 24
+};
+//プロパティの上書き
+val4.name = "曽村";
+//プロパティを追加
+val4.addres = "Gifu";
+
+console.log(val4);
+
+/**
+ * 配列
+ */
+
+// constで定義していても配列も上書きできる
+const val5 = ["dog", "cat"];
+// 配列の値の変更
+val5[0] = "bird";
+// 配列に値を追加
+val5.push("monkey");
+console.log(val5);
